bb5a1a65caac26d12f850c7a296af332ad6a4752
Parviz Sharipov <psharipov00@gmail.com>
2024-01-29 19:12:04 +0500

    fifth commit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,11 @@
-const video = document.getElementById("video");
-const circlePlayButton = document.getElementById("circle-play-b");
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
 
-function togglePlay() {
-    if (video.paused || video.ended) {
-        video.play();
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./imgs/1page/close_icon.svg"
     } else {
-        video.pause();
+navBtnImg.src = "./imgs/1page/open-icon.svg"
     }
 }
-
-circlePlayButton.addEventListener("click", togglePlay);
-video.addEventListener("playing", function () {
-    circlePlayButton.style.opacity = 0;
-});
-video.addEventListener("pause", function () {
-    circlePlayButton.style.opacity = 1;
-});
