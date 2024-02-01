9dae6d4cc3b703b2c6708f69d31f8df6437324af
Parviz Sharipov <psharipov00@gmail.com>
2024-02-01 12:56:14 +0500

    seventh commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const html = document.querySelector('body')
+
+
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
@@ -6,6 +9,36 @@ navBtn.onclick = () => {
     if (nav.classList.toggle('open')) {
         navBtnImg.src = "./imgs/1page/close_icon.svg"
     } else {
-navBtnImg.src = "./imgs/1page/open-icon.svg"
+        navBtnImg.src = "./imgs/1page/open-icon.svg"
     }
+}
+
+const findBtn = document.getElementById("find-btn");
+findBtn.onclick = function () {  
+    findBtn.innerHTML = `<div class="spinner-border" role="status">
+    <span class="visually-hidden">Загрузка...</span>
+  </div>`
+
+    setTimeout(()=>{
+      findBtn.innerHTML = 'Найти программу'
+      alert('text')
+    },1500)
+
+}
+
+
+
+const subscriptionBtn = document.getElementById('subscribe-btn')
+
+
+subscriptionBtn.onclick=()=>{
+    subscriptionBtn.innerHTML = `<div class="spinner-border text-secondary" role="status">
+    <span class="visually-hidden">Загрузка...</span>
+  </div>`
+
+    setTimeout(()=>{
+        subscriptionBtn.innerHTML = 'Вы подписаны'
+    },1500)
+    const emailValue = document.getElementById('email').value;
+    localStorage.setItem('email',emailValue)
 }
