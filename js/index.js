7ba3953037ad9b1d9d4270ad512e8257e740f496
Parviz Sharipov <psharipov00@gmail.com>
2024-02-02 22:30:33 +0500

    eightth commit

--- js/index.js
+++ js/index.js
@@ -6,39 +6,57 @@ const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
 
 navBtn.onclick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./imgs/1page/close_icon.svg"
-    } else {
-        navBtnImg.src = "./imgs/1page/open-icon.svg"
-    }
+  if (nav.classList.toggle('open')) {
+    navBtnImg.src = "./imgs/1page/close_icon.svg"
+  } else {
+    navBtnImg.src = "./imgs/1page/open-icon.svg"
+  }
 }
 
 const findBtn = document.getElementById("find-btn");
-findBtn.onclick = function () {  
-    findBtn.innerHTML = `<div class="spinner-border" role="status">
+findBtn.onclick = function () {
+  findBtn.innerHTML = `<div class="spinner-border" role="status">
     <span class="visually-hidden">Загрузка...</span>
   </div>`
 
-    setTimeout(()=>{
-      findBtn.innerHTML = 'Найти программу'
-      alert('text')
-    },1500)
+  setTimeout(() => {
+    findBtn.innerHTML = 'Найти программу'
+    alert('text')
+  }, 1500)
 
 }
-
-
 
 const subscriptionBtn = document.getElementById('subscribe-btn')
 
 
-subscriptionBtn.onclick=()=>{
-    subscriptionBtn.innerHTML = `<div class="spinner-border text-secondary" role="status">
+subscriptionBtn.onclick = () => {
+  subscriptionBtn.innerHTML = `<div class="spinner-border text-secondary" role="status">
     <span class="visually-hidden">Загрузка...</span>
   </div>`
 
-    setTimeout(()=>{
-        subscriptionBtn.innerHTML = 'Вы подписаны'
-    },1500)
-    const emailValue = document.getElementById('email').value;
-    localStorage.setItem('email',emailValue)
+  setTimeout(() => {
+    subscriptionBtn.innerHTML = 'Вы подписаны'
+  }, 1500)
+  const emailValue = document.getElementById('email').value;
+  const emails = localStorage.getItem('email') || [emailValue];
+  localStorage.setItem('email', emails)
 }
+
+const video = document.getElementById("video");
+const circlePlayButton = document.getElementById("circle-play-b");
+
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+circlePlayButton.addEventListener("click", togglePlay);
+video.addEventListener("playing", function () {
+  circlePlayButton.style.opacity = 0;
+});
+video.addEventListener("pause", function () {
+  circlePlayButton.style.opacity = 1;
+});
